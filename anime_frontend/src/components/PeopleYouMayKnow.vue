a033a1f19a81f0a9850558c95691c6ab4d263c02
Ramy Campusano Volquez <ramydcampusano@hotmail.com>
2023-11-28 20:45:47 -0600

    Database conflict solve 👍

--- anime_frontend/src/components/PeopleYouMayKnow.vue
+++ anime_frontend/src/components/PeopleYouMayKnow.vue
@@ -7,27 +7,7 @@
                 <div class="flex items-center space-x-2">
                     <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
                     
-                    <p class="text-xs"><strong>Code With Stein</strong></p>
-                </div>
-
-                <a href="#" class="py-2 px-3 bg-blue-600 text-white text-xs rounded-lg">Show</a>
-            </div>
-
-            <div class="flex items-center justify-between">
-                <div class="flex items-center space-x-2">
-                    <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-                    
-                    <p class="text-xs"><strong>Code With Stein</strong></p>
-                </div>
-
-                <a href="#" class="py-2 px-3 bg-blue-600 text-white text-xs rounded-lg">Show</a>
-            </div>
-
-            <div class="flex items-center justify-between">
-                <div class="flex items-center space-x-2">
-                    <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full">
-                    
-                    <p class="text-xs"><strong>Code With Stein</strong></p>
+                    <p class="text-xs"><strong>Coming Soon</strong></p>
                 </div>
 
                 <a href="#" class="py-2 px-3 bg-blue-600 text-white text-xs rounded-lg">Show</a>
